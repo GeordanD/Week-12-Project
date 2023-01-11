f8459904f11548e912ac472ed573e54c473a066b
Geordan Daugherty <geordan_daugherty@hotmail.com>
2023-01-10 20:57:49 -0600

    fixing

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ class AppointmentService {
     static getInfoFetch() {
         fetch(this.url)
             .then(response => response.json())
-            .then((data) => {
-                console.log(data[1]);
-                let appoint = data[0]
-                console.log(appoint)
-            })
+            // .then((data) => {
+            //     console.log(data[1]);
+            //     let appoint = data[0]
+            //     console.log(appoint)
+            // })
             
             .catch(err => console.log(err));
     }
@@ -64,7 +64,7 @@ class DOMManager {
         AppointmentService.createAppointment(new Appointment(Name, DateOfAppointment, TimeOfAppointment, ServicesCompleated)).then(() => {
             let Name = $("#Name").val();
             console.log(Name)
-            return AppointmentService.getAllAppointments();
+            return AppointmentService.LoadAllAppointments();
             
         })
             
@@ -75,26 +75,31 @@ class DOMManager {
         console.log(AppointmentService.deleteAppointment);
         AppointmentService.deleteAppointment(id)
             .done(() => {
-                return AppointmentService.getAllAppointments();
+                return AppointmentService.loadAllAppointments();
             })
-            .then((appointments) => this.render(appointments));
+            .then((appointments) => this.render(appointments))
+            .done(() => {
+                return DOMManager.LoadAllAppointments();
+        })
+
     }
 
   static render(appointments) {
-    this.appointments = appointments;
+      this.appointments = appointments;
+      fetch("https://63bb0bcecf99234bfa50f42b.mockapi.io/users");
     $("#app").empty();
     for (let appointment of appointments)
       $("#app").prepend(
         ` 
         
-        <div class="card bg-" id="${appointments.id}>
+        <div class="card  bg-dark text-danger" id="${appointments.id}>
         <div id="name" class="card-header">
         <h2>${appointment.Name}</h2>
-        <div id="date" class="card bg-info">${appointment.DateOfAppointment}</div>
-        <div id="time" class="card bg-info">${appointment.TimeOfAppointment}</div>
-        <div id="service" class="card bg-info">${appointment.ServicesCompleated}</div>
+        <div id="date" class="card bg-info text-dark">Date: ${appointment.DateOfAppointment}</div>
+        <div id="time" class="card bg-info text-dark">Time: ${appointment.TimeOfAppointment}</div>
+        <div id="service" class="card bg-info text-dark">Service: ${appointment.ServicesCompleated}</div>
         
-        <button class="btn btn-danger" onclick="DOMManager.deleteAppointment(${appointment.id})">Delete</button>
+        <button id="delete-btn" class="btn btn-danger" onclick="DOMManager.deleteAppointment(${appointment.id}), refreshPage();">Delete</button>
         </div>
         </div>
 
@@ -103,13 +108,19 @@ class DOMManager {
 console.log(appointments);
     }
   }
+function refreshPage() {
+    window.location.reload();
+}
 
+$("#delete-btn").click(() => {
+    DOMManager.loadAllAppointments(), refreshPage();
+})
 
 $("#create-new-appointment").click(() => {
   AppointmentService.createAppointment($("#Name, #DateOfAppointment, #TimeOfAppointment, #ServicesCompleated"));
     $("#Name, #DateOfAppointment, #TimeOfAppointment, #ServicesCompleated").val();{
         DOMManager.loadAllAppointments();
-    }
+    };
 TODO// fix loading issue after button is pressed to make appointment..
     
 });
@@ -117,32 +128,5 @@ TODO// fix loading issue after button is pressed to make appointment..
 AppointmentService.getInfoFetch();
 DOMManager.loadAllAppointments();
 console.log(AppointmentService.loadAllAppointments());
-console.log(AppointmentService.getAppointment(5));
+// console.log(AppointmentService.getAppointment());
 
-// `<div id="${user._id}" class="card">
-//                 <div class="card-header">
-//                 <h2>${user.name}</h2>
-//                 <button class="btn btn-danger" onclick="DOMManager.deleteAppointment('${Appointment._id}')">Delete</button>
-//                 <div class="card-body">
-//                 <div class="card">
-//                 <div class="row">
-//                 <div class="col-sm">
-//                 <input type="text" id="${user.id}-users-name" class="form-control" placeholder="Users Name">
-//                 <div class="col-sm">
-//                 <input type="date" id="${user.id}-users-date" class="form-control"
-//                 placeholder="Enter A Date"
-//                 </div>
-//                 <div class="col-sm">
-//                 <input type="time" id="${user.id}-users-time" class="form-control"
-//                 placeholder="Enter A Time"
-//                 </div>
-//                 </div>
-//                 <div class="col-sm">
-//                 <input type="text" id="${user.id}-services" class="form-control" placeholder="Services">
-//                 </div>
-//                 </div>
-//                 <button id="${user.id}-new-date" onclick="DOMManager.addDate('${user.id}')" class="btn btn-primary form-control">Add</button>
-//                 </div>
-//                 </div>
-//                 </div>
-//                 </div> <br>`;
